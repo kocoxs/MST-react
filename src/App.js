a22c1c50112e2934a57127a725aa6896a0127b35
Aaron Contreras <aaroncontreras1990@gmail.com>
2020-04-24 15:12:10 -0400

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import { getSnapshot } from "mobx-state-tree"
+import { observer } from 'mobx-react';
+import { values } from "mobx";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+//import { Todo, User } from './Models'
+
+//const john = User.create({name: "aaron"});
+//const eat = Todo.create();
+
+//console.log([getSnapshot(john), getSnapshot(eat)])
+
+const PrintJS = observer((item, snap) => {
+    
+    return (
+      <div>
+        <p>{JSON.stringify(item)}</p>
+        <p>{JSON.stringify(snap.todos)}</p>
+      </div>
+    )
+  }
+)
+
+class  App extends Component {
+  input = React.createRef();
+
+  submit = (event) => {
+    const name = this.input.current.value
+    const snap = getSnapshot(this.props.superStore)
+    const id = Object.keys(snap.todos).length + 1
+    
+    this.props.superStore.addTodo({id, name})
+    this.input.current.value = ""
+
+    console.log(getSnapshot(this.props.superStore))
+    event.preventDefault();
+  }
+
+  render () { 
+    const snap = getSnapshot(this.props.superStore)
+    return (
+      <div className="App">
+        <form  onSubmit={this.submit}>
+          <label>Nombre del Todo</label>
+          <br/>
+          <input 
+            type="text"
+            name="title"
+            ref={this.input}
+          />
+          <br/>
+          <button>Guardar</button>
+        </form>
+        <div>
+          <button onClick={this.previousState}>Anterior</button>
+          <button onClick={this.nextState} >siguiente</button>
+        </div>
+        <PrintJS item={this.props.superStore} snap={snap}/>
+      </div>
+    );
+  }
 }
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,45 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { getSnapshot, applySnapshot, onSnapshot } from "mobx-state-tree"
+import { RootStore } from './Models'
+
+const store = RootStore.create();
+const superStore = RootStore.create();
+
+store.addTodo({id: 1, name: 'aprender Moxb'})
+
+store.todos.get(1).toggle()
+//abteniendo el primer snapshot
+const getTheSnapShot1 = getSnapshot(store)
+//imprimir el primer snapshot
+//console.log(getTheSnapShot1)
+//adding second todo 
+store.addTodo({id: 2, name: 'la tarea que va de segunda'})
+//obteniendo el segundo snapshot
+const getTheSnapShot2 = getSnapshot(store)
+//imprimiendo snapshot 2 como se ve el store ahora
+//console.log(getTheSnapShot2);
+//actualizando el stado del store con un el snapshot1 
+applySnapshot(store, getTheSnapShot1)
+//obteniendo e imprimiendo el nuevo snapshot del store deberia ser el snapshot1
+const actualsnapshot = getSnapshot(store)
+//imprimiendo el snapshot
+//console.log(actualsnapshot)
+
+let states = []
+let currentFrame = -1
+
+onSnapshot(store, snapshot => {
+  if (currentFrame === states.length - 1) {
+      currentFrame++
+      states.push(snapshot)
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App superStore={superStore}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
